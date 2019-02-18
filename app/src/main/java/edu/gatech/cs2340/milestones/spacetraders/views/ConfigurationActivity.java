539f979aa085b52684c9e32165ba646ccaa4c3e7
ChongHyun Kim <dannykim124@gmail.com>
2019-02-17 20:18:51 -0500

    Create ConfigurationActivity.java

---
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/ConfigurationActivity.java
@@ -0,0 +1,10 @@
+package edu.gatech.cs2340.milestones.spacetraders.views;
+
+import android.support.v7.app.AppCompatActivity;
+
+import edu.gatech.cs2340.milestones.spacetraders.viewmodel.ConfigurationViewModel;
+
+public class ConfigurationActivity extends AppCompatActivity {
+
+    private ConfigurationViewModel viewModel;
+}
