c3adff46b8598ca1e19c2d2217f27da018be2049
ChongHyun Kim <dannykim124@gmail.com>
2019-02-18 00:58:16 -0500

    Update ConfigurationActivity.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/ConfigurationActivity.java
@@ -1,10 +1,24 @@
 package edu.gatech.cs2340.milestones.spacetraders.views;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import edu.gatech.cs2340.milestones.spacetraders.R;
+import edu.gatech.cs2340.milestones.spacetraders.entity.Player;
 import edu.gatech.cs2340.milestones.spacetraders.viewmodel.ConfigurationViewModel;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
     private ConfigurationViewModel viewModel;
+
+    private Player player;
+
+    private boolean editing;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_game);
+    }
+
 }
