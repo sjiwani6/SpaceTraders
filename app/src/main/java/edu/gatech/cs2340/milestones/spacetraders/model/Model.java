280202dd2cd29de8f0755cd365849ff57c478484
ChongHyun Kim <dannykim124@gmail.com>
2019-02-17 19:58:47 -0500

    Update Model.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/model/Model.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/model/Model.java
@@ -1,10 +1,17 @@
 package edu.gatech.cs2340.milestones.spacetraders.model;
 
+import edu.gatech.cs2340.milestones.spacetraders.entity.Player;
+
 public class Model {
     // player instance
     private Player player;
+    private  PlayerInteractor interactor;
 
     private static  Model instance = new Model();
-    public static Model getInstance() { return instance; }
+    public static Model getInstance() { return instance;}
+
+    public PlayerInteractor getPlayerInteractor() {
+        return interactor;
+    }
 
 }
