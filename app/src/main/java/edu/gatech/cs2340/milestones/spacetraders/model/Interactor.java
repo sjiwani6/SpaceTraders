64c3b3470bd77d2cc2c000ca805f397a3b8ba687
ChongHyun Kim <dannykim124@gmail.com>
2019-02-17 19:58:29 -0500

    Update Interactor.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/model/Interactor.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/model/Interactor.java
@@ -1,4 +1,14 @@
 package edu.gatech.cs2340.milestones.spacetraders.model;
 
 public abstract class Interactor {
+
+    private Repository myRepository;
+
+    protected Interactor(Repository repo) {
+        myRepository = repo;
+    }
+
+    protected Repository getRepository() {
+        return myRepository;
+    }
 }
