9c7ddbc0667cb2fa6d7b6b393258585c1280a9e2
ChongHyun Kim <dannykim124@gmail.com>
2019-02-18 13:06:38 -0500

    Update ConfigurationActivity.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/ConfigurationActivity.java
@@ -19,6 +19,7 @@ public class ConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game);
+
     }
 
 }
