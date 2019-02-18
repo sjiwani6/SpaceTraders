d9bcc3da864b538c537ae4b50a8368d176af3384
ChongHyun Kim <dannykim124@gmail.com>
2019-02-18 00:58:19 -0500

    Update MainActivity.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/MainActivity.java
@@ -2,15 +2,34 @@ package edu.gatech.cs2340.milestones.spacetraders.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import edu.gatech.cs2340.milestones.spacetraders.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View configView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        configView = getLayoutInflater().inflate(R.layout.new_game, null);
         setContentView(R.layout.activity_main);
 
+
+    }
+    public void onPressed(View view) {
+//        Button newGame = findViewById(R.id.newGame);
+        Log.d("msg", "you got here");
+        setContentView(configView);
+
+//        newGame.setOnClickListener( new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d("msg", "you got here");
+//            }
+//        });
     }
 }
