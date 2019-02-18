3978331b4f77698821db8ac3e2c2714e26326d64
ChongHyun Kim <dannykim124@gmail.com>
2019-02-17 20:18:58 -0500

    Create MainActivity.java

---
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.gatech.cs2340.milestones.spacetraders.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.milestones.spacetraders.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
