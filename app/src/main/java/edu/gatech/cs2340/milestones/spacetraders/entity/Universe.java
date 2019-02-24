3e6d199d6bc4945928328f7bce8b6a2e8f97748b
Roshan Siwakoti <roshan97@gatech.edu>
2019-02-24 18:02:03 -0500

    I have created a way to randomly generate the coordinates for the universe, and created Enums for the universes. still need to work on it to make sure the information is printed on logcat.

---
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Universe.java
@@ -0,0 +1,32 @@
+package edu.gatech.cs2340.milestones.spacetraders.entity;
+
+public enum Universe {
+    BOBERT("Bobert"),
+    NAMEK("Namek"),
+    SECTOR10("Sector10"),
+    CATOPIA("Catopia"),
+    PLANET_VEGETA ("Planet Vegeta"),
+    TONALD_DRUMP ("Tonald Drump"),
+    AMITA("Amita"),
+    NINEBALL("Nine Ball"),
+    ARAHAS("Ahrahas"),
+    SILVA("Silva");
+
+
+    private String universeName;
+
+    Universe(String diffLevel){
+        this.universeName = diffLevel;
+    }
+
+    public String getUniverse() {
+        return universeName;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
+
---
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/UniverseActivity.java
@@ -0,0 +1,47 @@
+package edu.gatech.cs2340.milestones.spacetraders.views;
+
+import android.support.v7.app.AppCompatActivity;
+import edu.gatech.cs2340.milestones.spacetraders.R;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class UniverseActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+        Log.d("here", "you got here_universe");
+
+       // button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+
+    }
+    public int xCoordinate(){
+        Random randX = new Random();
+        int rangeX = 101;
+        int x = randX.nextInt(rangeX);
+        return x;
+
+    }
+    public int yCoordinate(){
+        Random randY = new Random();
+        int rangeY = 151;
+        int y = randY.nextInt(rangeY);
+        return y;
+
+    }
+
+}
