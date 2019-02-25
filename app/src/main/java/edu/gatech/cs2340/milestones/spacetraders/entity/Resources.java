fb35ec63f395753ef8f0df2ca286af522fc85096
Roshan Siwakoti <roshan97@gatech.edu>
2019-02-24 23:02:58 -0500

    I have created a way to randomly generate the coordinates for the universe, and created Enums for the universes. still need to work on it to make sure the information is printed on logcat.

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Resources.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Resources.java
@@ -1,5 +1,32 @@
 package edu.gatech.cs2340.milestones.spacetraders.entity;
 
 enum Resources {
+    NOSPECIALRESOURCES("No Special Resources"),
+    MINERALRICH("Mineral Rich"),
+    MINERALPOOR("Mineral Poor"),
+    DESERT("Desert") ,
+    LOTSOFWATER("Lots of Water"),
+    RICHSOIL("Rich Soil"),
+    POORSOIL("Poor Soil"),
+    RICHFAUNA("Rich Fauna"),
+    LIFELESS("Lifeless"),
+    WEIRDMUSHROOMS("Weird Mushrooms"),
+    LOTSOFHERBS("Lots of Water"),
+    ARTISTIC("Lots of Herbs"),
+    WARLIKE("Warlike");
+
+    private String resources;
+
+    Resources(String techLevel) {
+        this.resources = techLevel;
+    }
+
+    public String getTechLevel() {
+        return resources;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
 }
