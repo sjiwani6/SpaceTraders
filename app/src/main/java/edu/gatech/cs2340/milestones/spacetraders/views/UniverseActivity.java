68737bb5b2e3932a58806ba21a899e56fd8e6964
Nitharjan <nkanthasamy3@gatech.edu>
2019-02-28 15:58:12 -0500

    Fixed minor issues

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/UniverseActivity.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/views/UniverseActivity.java
@@ -1,5 +1,6 @@
 package edu.gatech.cs2340.milestones.spacetraders.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import edu.gatech.cs2340.milestones.spacetraders.R;
 import edu.gatech.cs2340.milestones.spacetraders.entity.Universe;
@@ -11,21 +12,26 @@ import android.widget.Button;
 import java.util.Random;
 
 public class UniverseActivity extends AppCompatActivity {
-
+    private View startgame;
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //startgame = getLayoutInflater().inflate(R.layout.activity_startgame, null);
         setContentView(R.layout.activity_universe);
         Log.d("here", "you got here_universe");
 
         button = (Button) findViewById(R.id.UniverseButton);
 
+
     }
     public void onPressed(View view) {
         Universe universe = new Universe();
         Log.d("universe", universe.toString());
+        Intent intent = new Intent(this, StartGameActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
