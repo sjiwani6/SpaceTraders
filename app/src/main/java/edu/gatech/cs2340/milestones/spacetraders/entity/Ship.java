0ea26cbd5ecf4799f87c1df6c9a1756107d6d6cc
ChongHyun Kim <dannykim124@gmail.com>
2019-03-10 21:11:44 -0400

    Update Ship.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Ship.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Ship.java
@@ -1,18 +1,19 @@
 package edu.gatech.cs2340.milestones.spacetraders.entity;
 
 public class Ship {
-    private String shipName;
     private ShipType shipType;
+    //private Cargo cargo;
 
-    public Ship (String shipName) {
-        this(shipName, ShipType.GNAT);
+    public Ship () {
+        this.shipType = ShipType.GNAT;
     }
-    public Ship(String shipName, ShipType shipType) {
-        this.shipName = shipName;
-        this.shipType = shipType;
-    }
+
 
     public ShipType getShipType() {
         return shipType;
     }
+
+//    public Cargo getCargo() {
+//        return cargo;
+//    }
 }
