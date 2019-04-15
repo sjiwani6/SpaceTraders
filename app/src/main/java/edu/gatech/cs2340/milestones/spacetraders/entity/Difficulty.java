65424c3e718ab1cad050f84ba15b278fc1ff1ffa
Roshan Siwakoti <roshan97@gatech.edu>
2019-04-15 00:26:34 -0400

    Update Difficulty.java
    javadocs

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Difficulty.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/entity/Difficulty.java
@@ -1,5 +1,6 @@
 package edu.gatech.cs2340.milestones.spacetraders.entity;
 
+
 public enum Difficulty {
     EASY("Easy"),
     NORMAl("Normal"),
@@ -12,10 +13,18 @@ public enum Difficulty {
         this.diffLevel = diffLevel;
     }
 
+    /**
+     * This method is used to return the difficulty level.
+     * @return returns the difficulty level.
+     */
     public String getDiffLevel() {
         return diffLevel;
     }
 
+    /**
+     * This is the toString method for the diff level.
+     * @return a string representation of the difficulty level.
+     */
     @Override
     public String toString() {
         return super.toString();
