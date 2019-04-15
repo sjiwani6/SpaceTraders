255953437a30aa630dd5c361326d691c428b167f
Roshan Siwakoti <roshan97@gatech.edu>
2019-04-15 01:02:44 -0400

    Update ConfigurationViewModel.java

--- app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/viewmodel/ConfigurationViewModel.java
+++ app/src/main/java/edu/gatech/cs2340/milestones/spacetraders/viewmodel/ConfigurationViewModel.java
@@ -12,17 +12,38 @@ public class ConfigurationViewModel extends AndroidViewModel {
 
     private PlayerInteractor interactor;
 
+    /**
+     * this is the constructor for the class.
+     * @param application is the application parameter.
+     */
     public ConfigurationViewModel(@NonNull Application application) {
         super(application);
         interactor = Model.getInstance().getPlayerInteractor();
     }
+
+    /**
+     * this is the update method for the players.
+     * it takes a player as a param and update
+     * anything that is associated with the player.
+     * @param player takes in a player as a parameter.
+     */
     public void updatePlayer(Player player) {
         interactor.updatePlayer(player);
     }
+
+    /**
+     * this is the add method to add the
+     * player when they start the game.
+     * @param player takes in a player as a parameter.
+     */
     public void addPlayer(Player player) {
         interactor.addPlayer(player);
     }
 
+    /**
+     * this is the getter for the player.
+     * @return returns the player.
+     */
     public Player getPlayer() {
         return interactor.getPlayer();
     }
